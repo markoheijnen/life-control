14f5b4809fe2c94d4d9a4438061678d68e2bc631
Marko Heijnen <info@markoheijnen.com>
2014-09-29 09:46:44 +0300

    Move calling API from the series to the updater class.

--- inc/updater.php
+++ inc/updater.php
@@ -2,9 +2,90 @@
 
 class Life_Control_Updater {
 
-	public function __construct() {
-		//$this->check_empty_streamallthis();
+
+	public function load_serie( $imdb_id ) {
+		$url     = 'http://www.omdbapi.com/?plot=full&i=' . urlencode( $imdb_id );
+		$request = wp_remote_get( $url );
+		$body    = wp_remote_retrieve_body( $request );
+
+		if ( $body ) {
+			$data = json_decode( $body );
+
+			if ( $data ) {
+				return $data;
+			}
+		}
+
+		return false;
 	}
+
+	public function load_episodes( $tvrage_id ) {
+		$url     = 'http://services.tvrage.com/feeds/episode_list.php?sid=' . $tvrage_id;
+		$request = wp_remote_get( $url );
+		$body    = wp_remote_retrieve_body( $request );
+
+		if ( $body ) {
+			$data = simplexml_load_string( $body );
+
+			if ( $data ) {
+				$episodes = array();
+
+				foreach ( $data->Episodelist->Season as $season ) {
+					foreach ( $season->episode as $episode ) {
+						$episodes[] = array(
+							'season'    => (int)    $season['no'],
+							'episode'   => (int)    $episode->seasonnum,
+							'title'     => (string) $episode->title,
+							'date'      => (string) $episode->airdate,
+							'timestamp' => strtotime( $episode->airdate )
+						);
+					}
+				}
+
+				return $episodes;
+			}
+				
+		}
+
+		return false;
+	}
+
+
+	public function load_thumbnail( $post_id, $url, $desc = null ) {
+		if ( ! empty( $url ) ) {
+			// Download file to temp location
+			$tmp = download_url( $url );
+
+			// Set variables for storage
+			// fix file filename for query strings
+			preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $url, $matches);
+
+			if( isset( $matches[0] ) ) {
+				$file_array['name']     = basename( $matches[0] );
+			}
+
+			$file_array['tmp_name'] = $tmp;
+
+			// If error storing temporarily, unlink
+			if ( is_wp_error( $tmp ) ) {
+				@unlink( $file_array['tmp_name'] );
+				$file_array['tmp_name'] = '';
+			}
+
+			// do the validation and storage stuff
+			$id = media_handle_sideload( $file_array, $post_id, $desc );
+
+			if ( ! is_wp_error( $id ) ) {
+				return $id;
+			}
+
+			// If error storing permanently, unlink
+			@unlink( $file_array['tmp_name'] );
+		}
+
+		return false;
+	}
+
 
 	public function check_empty_streamallthis() {
 		$args = array(
--- posttypes/series.php
+++ posttypes/series.php
@@ -103,7 +103,7 @@ class Life_Control_Series {
 
 	public function wp_insert_post_data( $data, $postarr ) {
 		if ( ! $data['post_title'] && isset( $_POST['imdb_id'] ) ) {
-			$serie_data = $this->load_serie( $_POST['imdb_id'] );
+			$serie_data = Life_Control::$updater->load_serie( $_POST['imdb_id'] );
 
 			if ( $serie_data ) {
 				$data['post_title'] = $serie_data->Title;
@@ -150,7 +150,7 @@ class Life_Control_Series {
 		if ( $this->extra_meta_data ) {
 			foreach ( $this->extra_meta_data as $key => $value ) {
 				if ( 'thumbnail' ) {
-					$attachment_id = $this->load_thumbnail( $post_id, $value, $post->post_title );
+					$attachment_id = Life_Control::$updater->load_thumbnail( $post_id, $value, $post->post_title );
 
 					if ( $attachment_id ) {
             			set_post_thumbnail( $post_id, $attachment_id );
@@ -171,7 +171,7 @@ class Life_Control_Series {
 			$episodes = get_posts( $args );
 
 			if ( ! $episodes ) {
-				$episodes = $this->load_episodes( $tvrage_id );
+				$episodes = Life_Control::$updater->load_episodes( $tvrage_id );
 
 				if ( $episodes ) {
 					foreach ( $episodes as $episode ) {
@@ -203,91 +203,6 @@ class Life_Control_Series {
 				}
 			}
 		}
-	}
-
-
-
-	public function load_serie( $imdb_id ) {
-		$url     = 'http://www.omdbapi.com/?plot=full&i=' . urlencode( $imdb_id );
-		$request = wp_remote_get( $url );
-		$body    = wp_remote_retrieve_body( $request );
-
-		if ( $body ) {
-			$data = json_decode( $body );
-
-			if ( $data ) {
-				return $data;
-			}
-		}
-
-		return false;
-	}
-
-	public function load_episodes( $tvrage_id ) {
-		$url     = 'http://services.tvrage.com/feeds/episode_list.php?sid=' . $tvrage_id;
-		$request = wp_remote_get( $url );
-		$body    = wp_remote_retrieve_body( $request );
-
-		if ( $body ) {
-			$data = simplexml_load_string( $body );
-
-			if ( $data ) {
-				$episodes = array();
-
-				foreach ( $data->Episodelist->Season as $season ) {
-					foreach ( $season->episode as $episode ) {
-						$episodes[] = array(
-							'season'    => (int)    $season['no'],
-							'episode'   => (int)    $episode->seasonnum,
-							'title'     => (string) $episode->title,
-							'date'      => (string) $episode->airdate,
-							'timestamp' => strtotime( $episode->airdate )
-						);
-					}
-				}
-
-				return $episodes;
-			}
-				
-		}
-
-		return false;
-	}
-
-
-	public function load_thumbnail( $post_id, $url, $desc = null ) {
-		if ( ! empty( $url ) ) {
-			// Download file to temp location
-			$tmp = download_url( $url );
-
-			// Set variables for storage
-			// fix file filename for query strings
-			preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $url, $matches);
-
-			if( isset( $matches[0] ) ) {
-				$file_array['name']     = basename( $matches[0] );
-			}
-
-			$file_array['tmp_name'] = $tmp;
-
-			// If error storing temporarily, unlink
-			if ( is_wp_error( $tmp ) ) {
-				@unlink( $file_array['tmp_name'] );
-				$file_array['tmp_name'] = '';
-			}
-
-			// do the validation and storage stuff
-			$id = media_handle_sideload( $file_array, $post_id, $desc );
-
-			if ( ! is_wp_error( $id ) ) {
-				return $id;
-			}
-
-			// If error storing permanently, unlink
-			@unlink( $file_array['tmp_name'] );
-		}
-
-		return false;
 	}
 
 }
